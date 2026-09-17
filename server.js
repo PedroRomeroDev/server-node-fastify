4252dbbd305af0b46d33dbe53c28c533d65cf68a
Pedro Romero <romeropedrao2007@gmail.com>
2026-09-16 23:01:02 -0300

    Criação de Método de Busca por Data e DataMaior

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 import fastify from 'fastify'
+import games from "./games.json" with {type: 'json'}
+
 const server = fastify()
 
-server.get("/", (request, reply) => {
-    
+server.get("/jogos", (request, reply) => {
+    let resposta
+    const { dataMaior, dataLancamento } = request.query
+
+        if (dataMaior == "true"){
+            resposta = games.games
+            .filter(g => g.ano_lancamento > Number(dataLancamento))
+            .map(g => g.titulo)
+        } else if(dataMaior == "false"){
+            resposta = games.games
+            .filter(g => g.ano_lancamento < Number(dataLancamento))
+            .map(g => g.titulo)
+        } else if(dataLancamento){
+            resposta = games.games
+            .filter(g => g.ano_lancamento == Number(dataLancamento))
+            .map(g => g.titulo)
+        } else {
+            resposta = games.games
+            .map(g => g.titulo)
+        }
+
+    reply.send(resposta)
 })
 
 server.listen({ port: 3000 })
